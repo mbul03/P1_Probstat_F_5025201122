9315569041fd5b13b21ded8e224b2bc68a7dfe5e
mbul03 <72655925+mbul03@users.noreply.github.com>
2022-04-10 21:44:30 +0700

    Add files via upload

---
+++ Soal5.R
@@ -0,0 +1,21 @@
+# A
+lamda=3
+dexp(3)
+
+# B
+par(mfrow = c(2, 2))
+set.seed(1)
+hist(rexp(10), main = "saat n = 10")
+set.seed(1)
+hist(rexp(100), main = "saat n = 100")
+set.seed(1)
+hist(rexp(1000), main = "saat  = 1000")
+set.seed(1)
+hist(rexp(10000), main = "saat n = 10000")
+
+
+# C
+rata = 3
+cat ("Rataan(??) = ", rata, "\n")
+var = lamda*3
+cat ("Varian(??²) = ", var, "\n")
